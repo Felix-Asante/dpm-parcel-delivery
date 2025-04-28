fc6a7c3b8ed37bc46386c1ff99b4ca2ff5ef2447
Felix-Asante <webdevwithfelix@gmail.com>
2025-04-28 09:49:24 +0100

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+
   env: {
     schema: {
       API_BASE_URL: envField.string({ context: "client", access: "public" }),
     },
   },
+
+  adapter: vercel(),
 });
